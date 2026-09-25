85bdcdf5e51d85ed909f381d40a29269f6a1191e
Tom Kelsey <thomas.e.kelsey@student.uts.edu.au>
2026-09-25 13:37:02 +1000

    DoBot6 initialiser

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ import swift
 # Import required files
 from create_static_environment import create_static_environment
 
+# Import robot models
+from RS007N import RS007N
+from DoBot6 import DoBot6
+
 def main():
 	"""Start Swift and build the static project environment."""
 	env = swift.Swift()
 	env.launch(realtime=True)
 	create_static_environment(env)
 	env.hold()
+
+
 
 if __name__ == "__main__":
 	main()
